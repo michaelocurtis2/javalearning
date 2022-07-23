1d55988b4b9181e38613cd2e36fd7deaa6ec5743
Michael <1902276@student.uwtsd.ac.uk>
2022-07-23 04:11:02 +0100

    Add icon to window

--- src/windowframe/WindowFrame.java
+++ src/windowframe/WindowFrame.java
@@ -2,6 +2,10 @@ package windowframe;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+import java.lang.constant.Constable;
+import java.net.URL;
+import java.util.Objects;
 
 public class WindowFrame {
     public static void main(String[] args) {
@@ -10,10 +14,14 @@ public class WindowFrame {
 
     public static void createWindow() {
         JFrame frame = new JFrame("Frame");
-        JLabel emptyLabel = new JLabel("Frame");
+        JLabel emptyLabel = new JLabel("Hello World!");
+
+        ClassLoader classLoader = WindowFrame.class.getClassLoader();
+        ImageIcon img = new ImageIcon(Objects.requireNonNull(classLoader.getResource("windowframe/resources/icon.png")));
 
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+        frame.setIconImage(img.getImage());
 
         //Display window
         frame.setSize(320, 180);
